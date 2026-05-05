ed76bcd0066bc4c2b86f23fc44b84c8b72ed5ff9
Felipe Corrêa <correagfe@gmail.com>
2026-05-04 21:35:58 -0300

    update

--- frontend/src/components/Popup.jsx
+++ frontend/src/components/Popup.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import styles from './Popup.module.css' 
+import { createPortal } from "react-dom";
+import styles from './Popup.module.css';
 
 function Popup(props) {
-    return (props.trigger) ? (
+    // Se trigger for true, renderiza o portal direto no body da página
+    return (props.trigger) ? createPortal(
         <div className={styles.popup}>
             <div className={styles.popupinner}>
-                <button className={styles.closebtn} onClick={() => {props.setTrigger(false); props.setTextEdit(false); props.setScenario(false)}}>&#10006;</button>
+                <button 
+                    className={styles.closebtn} 
+                    onClick={() => {
+                        props.setTrigger(false); 
+                        // Verificações de segurança adicionadas caso você use esse 
+                        // mesmo Popup em lugares que não passam essas props
+                        if(props.setTextEdit) props.setTextEdit(false); 
+                        if(props.setScenario) props.setScenario(false);
+                    }}
+                >
+                    &#10006;
+                </button>
                 { props.children }
             </div>
-        </div>
+        </div>,
+        document.body // <-- Elemento de destino do Portal
     ) : "";
 }
 
-export default Popup
+export default Popup;
