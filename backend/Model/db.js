f81d25cd1c8589ed4885815643cf91419e486c25
Felipe Corrêa <correagfe@gmail.com>
2026-07-10 11:50:42 -0300

    update

--- backend/Model/db.js
+++ backend/Model/db.js
@@ -1,3 +1,18 @@
+// const mysql = require('mysql2/promise');
+// require('dotenv').config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// module.exports = pool;
+
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
@@ -6,9 +21,14 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
